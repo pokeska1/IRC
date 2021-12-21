1ecd1b80260cdf12f5859932d3fbacee2685b888
RuslanBairashev <ruslan.bairashev@gmail.com>
2021-12-21 13:31:20 +0300

    TODO command list

---
+++ Channel.hpp
@@ -0,0 +1,24 @@
+#ifndef CHANNEL_HPP
+#define CHANNEL_HPP
+#include <iostream>
+#include <vector>
+
+class Channel
+{
+private:
+	std::vector<int>	chan_list;
+public:
+	Channel(/* args */);
+	~Channel();
+};
+
+Channel::Channel(/* args */)
+{
+}
+
+Channel::~Channel()
+{
+}
+
+
+#endif
