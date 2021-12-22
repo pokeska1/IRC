74e7031b86b3e006c35b460325f02c76d66e51f2
RuslanBairashev <ruslan.bairashev@gmail.com>
2021-12-22 11:42:58 +0300

    Client->User

--- User.cpp
+++ User.cpp
@@ -3,16 +3,24 @@
 User::User(std::string	str):nickname(str) {}
 User::~User() {}
 //Getters
+int const		&User::getFd() const { return(this->fd); }
 std::string	User::getNickname() const { return nickname; }
-std::string	User::getPassword() const { return password; }
+std::string	User::getPassword_network() const { return password_network; }
 std::string	User::getUsername() const { return username; }
 std::string	User::getHostname() const { return hostname; }
 std::string	User::getServername() const { return servername; }
 std::string	User::getRealname() const { return realname; }
+bool const	&User::getPassword_init() const { return(this->password); }
+bool const	&User::getName_init() const { return(this->name_init); }
+bool const	&User::getAccess() const { return(this->access); }
+
 //Setters
 void		User::setNickname(std::string str) { nickname = str; }
-void		User::setPassword(std::string str) { password = str; }
+void		User::setPassword_network(std::string str) { password_network = str; }
 void		User::setUsername(std::string str) { username = str; }
 void		User::setHostname(std::string str) { hostname = str; }
 void		User::setServername(std::string str) { servername = str; }
 void		User::setRealname(std::string str) { realname = str; }
+void		User::setPassword_init(bool password) { this->password = password; }
+void		User::setName_init(bool name_init) { this->name_init = name_init; }
+void		User::setAccess(bool access) { this->access = access; }
--- User.hpp
+++ User.hpp
@@ -3,15 +3,43 @@
 #include <iostream>
 #include <vector>
 
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
+
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/poll.h>
+#include <sys/select.h>
+#include <vector>
+#include <iostream>
+#include <string>
+
+
+#define BUFLEN 512
+#define PORT 5555
+
 class User
 {
 private:
-	std::string			nickname;
-	std::string			password;
-	std::string			username;
-	std::string			hostname;
-	std::string			servername;
-	std::string			realname;
+	int				fd;
+	std::string		nickname;
+	std::string		password_network;
+	std::string		username;
+	std::string		hostname;
+	std::string		servername;
+	std::string		realname;
+	bool			password;
+	bool			name_init;
+	bool			access;
 
 	User(const User &cp);
 	void	operator=(const User &cp);
@@ -19,17 +47,24 @@ public:
 	User(std::string	str);
 	~User();
 	//Getters
+	int	const	&getFd() const;
 	std::string	getNickname() const;
-	std::string	getPassword() const;
+	std::string	getPassword_network() const;
 	std::string	getUsername() const;
 	std::string	getHostname() const;
 	std::string	getServername() const;
 	std::string	getRealname() const;
+	bool const	&getPassword_init() const;
+	bool const	&getName_init() const;
+	bool const	&getAccess() const;
 	//Setters
 	void		setNickname(std::string str);
-	void		setPassword(std::string str);
+	void		setPassword_network(std::string str);
 	void		setUsername(std::string str);
 	void		setHostname(std::string str);
 	void		setServername(std::string str);
 	void		setRealname(std::string str);
+	void		setPassword_init(bool password);
+	void		setName_init(bool name_init);
+	void		setAccess(bool access);
 };
